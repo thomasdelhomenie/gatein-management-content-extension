eddc6c1448054745578117d6e794ad1c1b1099dc
thomasdelhomenie <thomas.delhomenie@gmail.com>
2012-07-26 10:58:57 +0200

    Metadata file export for metadata templates

--- src/main/java/org/exoplatform/management/content/operations/templates/metadata/MetadataTemplatesExportResource.java
+++ src/main/java/org/exoplatform/management/content/operations/templates/metadata/MetadataTemplatesExportResource.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.exoplatform.management.content.exporttask.StringExportTask;
+import org.exoplatform.management.content.operations.templates.NodeTemplate;
 import org.exoplatform.services.cms.BasePath;
 import org.exoplatform.services.cms.metadata.MetadataService;
+import org.exoplatform.services.cms.metadata.impl.MetadataServiceImpl;
 import org.exoplatform.services.jcr.ext.hierarchy.NodeHierarchyCreator;
 import org.gatein.management.api.exceptions.OperationException;
 import org.gatein.management.api.operation.OperationContext;
@@ -24,6 +26,7 @@ public class MetadataTemplatesExportResource implements OperationHandler {
 	
 	private MetadataService metadataService;
 	private NodeHierarchyCreator nodeHierarchyCreator;
+	private MetadataTemplatesMetaData metadata;
 	
 	@Override
 	public void execute(OperationContext operationContext, ResultHandler resultHandler) throws OperationException {
@@ -34,21 +37,33 @@ public class MetadataTemplatesExportResource implements OperationHandler {
 
 		metadataService = operationContext.getRuntimeContext().getRuntimeComponent(MetadataService.class);
 		nodeHierarchyCreator = operationContext.getRuntimeContext().getRuntimeComponent(NodeHierarchyCreator.class);
-
+	
 		try {
-			// MetadataService does not expose the templates base node path...
+			// MetadataService does not expose the templates base node path, "internal" method is used...
 			String templatesBasePath = nodeHierarchyCreator.getJcrPath(BasePath.METADATA_PATH);
 			
+			// loop over all metadata and export templates. Only views and dialogs exist for metadata templates, no skins templates.
 			boolean[] isDialogValues = new boolean[]{true, false};
 			List<String> metadataList = metadataService.getMetadataList();
 			for(String metadataName : metadataList) {	
+				metadata = new MetadataTemplatesMetaData();
+				
+				// TODO label is not exposed by the API...
+				metadata.setLabel("");
+				// metadata templates are not document templates
+				metadata.setDocumentTemplate(false);
+				
 				for(boolean isDialog : isDialogValues) {
 					String metadataPath = metadataService.getMetadataPath(metadataName, isDialog);
 					String metadataRoles = metadataService.getMetadataRoles(metadataName, isDialog);
 					String metadataTemplate = metadataService.getMetadataTemplate(metadataName, isDialog);
+					String templatePath = metadataPath.substring(templatesBasePath.length());
+										
+					exportTasks.add(new StringExportTask(metadataTemplate, EXPORT_BASE_PATH + templatePath + ".gtmpl"));
 					
-					exportTasks.add(new StringExportTask(metadataTemplate, EXPORT_BASE_PATH + metadataPath.substring(templatesBasePath.length()) + ".gtmpl"));
+					metadata.addTemplate(isDialog ? MetadataServiceImpl.DIALOGS : MetadataServiceImpl.VIEWS, new NodeTemplate(templatePath + ".gtmpl", metadataRoles));
 				}
+				exportTasks.add(new MetadataTemplatesMetaDataExportTask(metadata, EXPORT_BASE_PATH + "/" + metadataName));
 			}			
 		} catch (Exception e) {
 			throw new OperationException(operationName, "Error while retrieving node types templates", e);
