378002eddbb6c66c16bf2179a5a8b6cbb68f28c6
boubaker <boubaker.khanfir@gmail.com>
2012-09-27 17:49:47 +0200

    Add Managed resources: script, actiontype, drive, nodetype

--- src/main/java/org/exoplatform/management/content/ContentManagementExtension.java
+++ src/main/java/org/exoplatform/management/content/ContentManagementExtension.java
@@ -2,11 +2,19 @@ package org.exoplatform.management.content;
 
 import org.exoplatform.management.content.operations.ContentReadResource;
 import org.exoplatform.management.content.operations.EmptyReadResource;
+import org.exoplatform.management.content.operations.action.ActionExportResource;
+import org.exoplatform.management.content.operations.action.ActionReadResource;
 import org.exoplatform.management.content.operations.drive.DriveExportResource;
 import org.exoplatform.management.content.operations.drive.DriveImportResource;
 import org.exoplatform.management.content.operations.drive.DriveReadResource;
+import org.exoplatform.management.content.operations.nodetype.NodeTypeExportResource;
+import org.exoplatform.management.content.operations.nodetype.NodeTypeImportResource;
+import org.exoplatform.management.content.operations.nodetype.NodeTypeReadResource;
 import org.exoplatform.management.content.operations.queries.QueriesExportResource;
 import org.exoplatform.management.content.operations.queries.QueriesReadResource;
+import org.exoplatform.management.content.operations.script.ScriptExportResource;
+import org.exoplatform.management.content.operations.script.ScriptImportResource;
+import org.exoplatform.management.content.operations.script.ScriptReadResource;
 import org.exoplatform.management.content.operations.site.LiveSitesReadResource;
 import org.exoplatform.management.content.operations.site.SiteReadResource;
 import org.exoplatform.management.content.operations.site.contents.SiteContentsExportResource;
@@ -47,77 +55,80 @@ public class ContentManagementExtension implements ManagementExtension {
     content.registerOperationHandler(OperationNames.READ_RESOURCE, new ContentReadResource(),
         description("Lists available contents data"));
     content.registerOperationHandler(OperationNames.IMPORT_RESOURCE, new SiteContentsImportResource(),
-            description("Import contents data"));
+        description("Import contents data"));
 
     // /content/sites
-    ManagedResource.Registration sites = content
-        .registerSubResource("sites", description("Sites Managed Resource, responsible for handling management operations on sites contents."));
+    ManagedResource.Registration sites = content.registerSubResource("sites",
+        description("Sites Managed Resource, responsible for handling management operations on sites contents."));
     sites.registerOperationHandler(OperationNames.READ_RESOURCE, new LiveSitesReadResource(),
         description("Lists available sites"));
 
     // /content/sites/<site_name>
-    ManagedResource.Registration site = sites.registerSubResource("{site-name: .*}", description("Management resource responsible for handling management operations on a specific site."));
+    ManagedResource.Registration site = sites.registerSubResource("{site-name: .*}",
+        description("Management resource responsible for handling management operations on a specific site."));
     site.registerOperationHandler(OperationNames.READ_RESOURCE, new SiteReadResource(), description("Read site"));
-    
+
     // /content/sites/<site_name>/contents
-    ManagedResource.Registration siteContents = site.registerSubResource("contents", description("Management resource responsible for handling management operations on contents of a specific site."));
-    siteContents.registerOperationHandler(OperationNames.READ_RESOURCE, new SiteContentsReadResource(), description("Read site contents"));
+    ManagedResource.Registration siteContents = site.registerSubResource("contents",
+        description("Management resource responsible for handling management operations on contents of a specific site."));
+    siteContents.registerOperationHandler(OperationNames.READ_RESOURCE, new SiteContentsReadResource(),
+        description("Read site contents"));
     siteContents.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new SiteContentsExportResource(),
-        description("Export site contents"));    
-    
+        description("Export site contents"));
+
     // /content/sites/<site_name>/seo
-    ManagedResource.Registration seo = site.registerSubResource("seo", description("Management resource responsible for handling management operations on SEO of a specific site."));
+    ManagedResource.Registration seo = site.registerSubResource("seo",
+        description("Management resource responsible for handling management operations on SEO of a specific site."));
     seo.registerOperationHandler(OperationNames.READ_RESOURCE, new SiteSEOReadResource(), description("Read site SEO data"));
-    seo.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new SiteSEOExportResource(),
-        description("Export site SEO data"));    
+    seo.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new SiteSEOExportResource(), description("Export site SEO data"));
 
     // /content/templates
-    ManagedResource.Registration templates = content
-        .registerSubResource("templates", description("Sites Managed Resource, responsible for handling management operations on templates."));
+    ManagedResource.Registration templates = content.registerSubResource("templates",
+        description("Sites Managed Resource, responsible for handling management operations on templates."));
     templates.registerOperationHandler(OperationNames.READ_RESOURCE, new TemplatesReadResource(),
         description("Lists available template types"));
 
     // /content/templates/applications
-    ManagedResource.Registration applicationsTemplates = templates
-        .registerSubResource("applications", description("Sites Managed Resource, responsible for handling management operations on applications templates."));
+    ManagedResource.Registration applicationsTemplates = templates.registerSubResource("applications",
+        description("Sites Managed Resource, responsible for handling management operations on applications templates."));
     applicationsTemplates.registerOperationHandler(OperationNames.READ_RESOURCE, new ApplicationsTemplatesReadResource(),
         description("Lists available applications containing templates"));
 
     // /content/templates/applications/<application_name>
-    ManagedResource.Registration applicationTemplates = applicationsTemplates
-        .registerSubResource("{application-name: .*}", description("Sites Managed Resource, responsible for handling management operations on templates of an application."));
+    ManagedResource.Registration applicationTemplates = applicationsTemplates.registerSubResource("{application-name: .*}",
+        description("Sites Managed Resource, responsible for handling management operations on templates of an application."));
     applicationTemplates.registerOperationHandler(OperationNames.READ_RESOURCE, new ApplicationTemplatesReadResource(),
         description("Lists available templates of an application"));
     applicationTemplates.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new ApplicationTemplatesExportResource(),
-            description("Exports available templates of an application"));  
-    
+        description("Exports available templates of an application"));
+
     // /content/templates/nodetypes
-    ManagedResource.Registration nodetypesTemplates = templates
-        .registerSubResource("nodetypes", description("Sites Managed Resource, responsible for handling management operations on node types templates."));
+    ManagedResource.Registration nodetypesTemplates = templates.registerSubResource("nodetypes",
+        description("Sites Managed Resource, responsible for handling management operations on node types templates."));
     nodetypesTemplates.registerOperationHandler(OperationNames.READ_RESOURCE, new NodeTypesTemplatesReadResource(),
         description("Lists available node types templates"));
     nodetypesTemplates.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new NodeTypesTemplatesExportResource(),
-            description("Exports available node types templates"));
-    
+        description("Exports available node types templates"));
+
     // /content/templates/metadata
-    ManagedResource.Registration metadataTemplates = templates
-        .registerSubResource("metadata", description("Sites Managed Resource, responsible for handling management operations on metadata templates."));
+    ManagedResource.Registration metadataTemplates = templates.registerSubResource("metadata",
+        description("Sites Managed Resource, responsible for handling management operations on metadata templates."));
     metadataTemplates.registerOperationHandler(OperationNames.READ_RESOURCE, new MetadataTemplatesReadResource(),
         description("Lists available metadata templates"));
     metadataTemplates.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new MetadataTemplatesExportResource(),
-            description("Exports available metadata templates"));    
-    
+        description("Exports available metadata templates"));
+
     // /content/queries
-    ManagedResource.Registration queries = content
-        .registerSubResource("queries", description("Queries Managed Resource, responsible for handling management operations on queries."));
+    ManagedResource.Registration queries = content.registerSubResource("queries",
+        description("Queries Managed Resource, responsible for handling management operations on queries."));
     queries.registerOperationHandler(OperationNames.READ_RESOURCE, new QueriesReadResource(),
         description("Lists available queries"));
     queries.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new QueriesExportResource(),
-            description("Exports available queries"));
+        description("Exports available queries"));
 
     // /content/taxonomy
-    ManagedResource.Registration taxonomies = content
-        .registerSubResource("taxonomy", description("Taxonomy Managed Resource, responsible for handling management operations on taxonomies."));
+    ManagedResource.Registration taxonomies = content.registerSubResource("taxonomy",
+        description("Taxonomy Managed Resource, responsible for handling management operations on taxonomies."));
     taxonomies.registerOperationHandler(OperationNames.READ_RESOURCE, new TaxonomyReadResource(),
         description("Lists available taxonomies"));
     taxonomies.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new TaxonomyExportResource(),
@@ -125,23 +136,48 @@ public class ContentManagementExtension implements ManagementExtension {
     taxonomies.registerOperationHandler(OperationNames.IMPORT_RESOURCE, new TaxonomyImportResource(),
         description("Imports available taxonomies"));
 
+    // /content/script
+    ManagedResource.Registration script = content.registerSubResource("script",
+        description("ECMS script Managed Resource, responsible for handling management operations on ECMS scripts."));
+    script.registerOperationHandler(OperationNames.READ_RESOURCE, new ScriptReadResource(),
+        description("Lists available scripts"));
+    script.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new ScriptExportResource(),
+        description("Exports available scripts"));
+    script.registerOperationHandler(OperationNames.IMPORT_RESOURCE, new ScriptImportResource(), description("Imports scripts"));
+
+    // /content/action
+    ManagedResource.Registration action = content.registerSubResource("action",
+        description("Action Managed Resource, responsible for handling management operations on actions."));
+    action.registerOperationHandler(OperationNames.READ_RESOURCE, new ActionReadResource(),
+        description("Lists available actions"));
+    action.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new ActionExportResource(),
+        description("Exports available actions"));
+    action.registerOperationHandler(OperationNames.IMPORT_RESOURCE, new NodeTypeImportResource(), description("Imports actions"));
+
+    // /content/nodetype
+    ManagedResource.Registration nodetype = content.registerSubResource("nodetype",
+        description("Nodetype Managed Resource, responsible for handling management operations on nodetypes."));
+    nodetype.registerOperationHandler(OperationNames.READ_RESOURCE, new NodeTypeReadResource(),
+        description("Lists available actions"));
+    nodetype.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new NodeTypeExportResource(),
+        description("Exports available actions"));
+    nodetype.registerOperationHandler(OperationNames.IMPORT_RESOURCE, new NodeTypeImportResource(),
+        description("Imports actions"));
+
     // /content/drive
-    ManagedResource.Registration drive = content
-        .registerSubResource("drive", description("ECMS Drive Managed Resource, responsible for handling management operations on ECMS Drives."));
-    drive.registerOperationHandler(OperationNames.READ_RESOURCE, new DriveReadResource(),
-        description("Lists available drives"));
+    ManagedResource.Registration drive = content.registerSubResource("drive",
+        description("ECMS Drive Managed Resource, responsible for handling management operations on ECMS drives."));
+    drive.registerOperationHandler(OperationNames.READ_RESOURCE, new DriveReadResource(), description("Lists available drives"));
     drive.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new DriveExportResource(),
         description("Exports available drives"));
-    drive.registerOperationHandler(OperationNames.IMPORT_RESOURCE, new DriveImportResource(),
-        description("Imports available drives"));
-    
+    drive.registerOperationHandler(OperationNames.IMPORT_RESOURCE, new DriveImportResource(), description("Imports drives"));
+
     // /content/taxonomy/name
-    ManagedResource.Registration taxonomy = taxonomies
-        .registerSubResource("{taxonomy-name: .*}", description("Taxonomy Managed Resource, responsible for handling management operations on taxonomies."));
-    taxonomy.registerOperationHandler(OperationNames.READ_RESOURCE, new EmptyReadResource(),
-        description("nothing"));
+    ManagedResource.Registration taxonomy = taxonomies.registerSubResource("{taxonomy-name: .*}",
+        description("Taxonomy Managed Resource, responsible for handling management operations on taxonomies."));
+    taxonomy.registerOperationHandler(OperationNames.READ_RESOURCE, new EmptyReadResource(), description("nothing"));
     taxonomy.registerOperationHandler(OperationNames.EXPORT_RESOURCE, new TaxonomyExportResource(),
-            description("Exports selected taxonomy"));
+        description("Exports selected taxonomy"));
   }
 
   @Override
