a03722ce0f2e5f10d1ffe972bc566bc54fb21701
thomasdelhomenie <thomas.delhomenie@gmail.com>
2012-09-28 15:45:36 +0200

    Remove unused import

--- src/test/java/org/exoplatform/management/content/test/operations/queries/TestQueriesExportTask.java
+++ src/test/java/org/exoplatform/management/content/test/operations/queries/TestQueriesExportTask.java
@@ -1,28 +1,10 @@
 package org.exoplatform.management.content.test.operations.queries;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import org.custommonkey.xmlunit.Diff;
-import org.custommonkey.xmlunit.XMLUnit;
-import org.exoplatform.container.xml.ComponentPlugin;
 import org.exoplatform.container.xml.Configuration;
-import org.exoplatform.container.xml.ExternalComponentPlugins;
-import org.exoplatform.container.xml.InitParams;
-import org.exoplatform.container.xml.ObjectParameter;
 import org.exoplatform.management.content.operations.queries.QueriesExportTask;
-import org.exoplatform.services.cms.queries.QueryService;
-import org.exoplatform.services.cms.queries.impl.QueryData;
-import org.exoplatform.services.cms.queries.impl.QueryPlugin;
-import org.gatein.management.api.operation.model.ExportTask;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 
 /**
